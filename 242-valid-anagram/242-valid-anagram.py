c0b34cd267d3edb27f49efbd1c85f62a0693d656
kayode-gif <s2020476@ed.ac.uk>
2022-06-17 16:38:17 +0100

    Time: 75 ms (45.59%), Space: 14.5 MB (66.04%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        s_map = {}
+        t_map = {}
+        for i in range(len(s)):
+            s_map[s[i]] = 1 + s_map.get(s[i],0)
+            t_map[t[i]] = 1 + t_map.get(t[i],0)
+        for count in s_map:
+            if(s_map[count] != t_map.get(count,0)):
+                return False
+        return True
+                
 
             
             
