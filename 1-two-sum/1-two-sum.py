724bebd1a1dd11e069e21cfbc3adff28aae27ca9
kayode-gif <s2020476@ed.ac.uk>
2022-06-17 16:42:37 +0100

    Time: 6864 ms (7.05%), Space: 14.8 MB (95.00%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,19 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #loop through array if first number doesnt link
         for i in range(0,len(nums)):
-            #loop through number after first index to find other number
             for j in range(i+1,len(nums)):
-                #if the target - first == second other number
                 if(target - nums[i] == nums[j]):
-                    #return the index of it 
                     return [i,j]
-            
-                    
-                
-            
-        
-                
-                
-                
-            
