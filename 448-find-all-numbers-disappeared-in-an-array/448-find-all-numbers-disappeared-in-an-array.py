03400f2f0ad6dae236b976f15fbe29672e754513
kayode-gif <s2020476@ed.ac.uk>
2022-06-18 12:53:46 +0100

    Time: 730 ms (7.64%), Space: 24.6 MB (33.88%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -3,8 +3,10 @@ class Solution:
         #stores the missing numbers 
         arrayM = []
         numSet = set()
+        #loop through if number in set add it
         for i in nums:
             numSet.add(i)
+        #now loop through [1,n] if its not add it into arrayM
         for i in range(1,len(nums)+1):
             if (i not in numSet):
                 arrayM.append(i)
