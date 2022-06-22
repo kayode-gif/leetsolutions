77a578c500e97bcc83fa572e98c413d9a04b297e
kayode-gif <s2020476@ed.ac.uk>
2022-06-22 16:15:31 +0100

    Time: 2343 ms (5.01%), Space: 25.1 MB (6.56%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -9,12 +9,17 @@ class Solution:
 #         return profit
                 
 #O(N) SOLUTION 
+        #holds profit
         maxP = 0
+        #set minimum buy to the first value
         buy = prices[0]
+        #loop through the array looking for "current" values less than min
         for i in range(1,len(prices)):
             if(prices[i] < buy):
+                #if a new minimum is found, buy it!
                 buy = prices[i]
             else:
+                #if a new minimum isnt, return 0 or diff between sold - min buy
                 maxP = max(maxP, prices[i] - buy)
         return maxP
                 
