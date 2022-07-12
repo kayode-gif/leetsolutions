12c0ee24608324d7d02fdee4a121d0b32c32c072
kayode-gif <s2020476@ed.ac.uk>
2022-07-12 19:58:05 +0100

    Time: 29 ms (96.03%), Space: 13.9 MB (75.43%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,15 +6,16 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
         queue = [(p, q)]
-        
         while queue:
             node_p, node_q = queue.pop(0)
+            #if both of nodes have values continue 
             if not node_p and not node_q:
                 continue
+            #if atleast one of the nodes differ return False
             if not node_p or not node_q:
                 return False
+            #if the values are the same append both of them to the queue
             if node_p.val == node_q.val:
                 queue.append((node_p.left, node_q.left))
                 queue.append((node_p.right, node_q.right))
