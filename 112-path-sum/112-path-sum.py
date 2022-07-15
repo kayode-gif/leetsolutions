3b96f8cbd0351b9b032720a400571a49a010c913
kayode-gif <s2020476@ed.ac.uk>
2022-07-15 16:29:00 +0100

    Time: 51 ms (80.83%), Space: 15.1 MB (57.60%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -7,16 +7,27 @@
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         #iterative dfs 
+        # if not root:
+        #     return False
+        # stack = [(root,targetSum - root.val)]
+        # while stack:
+        #     node,value = stack.pop()
+        #     if not node.left and not node.right and value == 0:
+        #         return True
+        #     if node.left != None:
+        #         stack.append((node.left,value - node.left.val))
+        #     if node.right != None:
+        #         stack.append((node.right,value - node.right.val))
+        # return False
+    
+        #recursive dfs 
         if not root:
             return False
-        stack = [(root,targetSum - root.val)]
-        while stack:
-            node,value = stack.pop()
-            if not node.left and not node.right and value == 0:
-                return True
-            if node.left != None:
-                stack.append((node.left,value - node.left.val))
-            if node.right != None:
-                stack.append((node.right,value - node.right.val))
-        return False
-                
+        if root.left == None and root.right == None:
+            return root.val == targetSum
+        return self.hasPathSum(root.left,targetSum - root.val) or self.hasPathSum(root.right,targetSum - root.val)
+            
+            
+        
+        
+        
