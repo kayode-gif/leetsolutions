0d0a6623e8e7889d5a2e25674ba94eb7419b22f8
kayode-gif <s2020476@ed.ac.uk>
2022-07-15 13:46:59 +0100

    Time: 1179 ms (7.44%), Space: 54.9 MB (30.23%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -6,16 +6,30 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+        
+        #iterative dfs 
+        # if not root:
+        #     return 0
+        # stack = [(root,1)]
+        # curr_depth = float('inf')
+        # while stack:
+        #     node,depth = stack.pop()
+        #     if not node.right and not node.left:
+        #         curr_depth = min(curr_depth,depth)
+        #     if node.right:
+        #         stack.append((node.right,depth+1))
+        #     if node.left:
+        #         stack.append((node.left,depth+1))
+        # return curr_depth
+        
+        #recursive dfs 
         if not root:
             return 0
-        stack = [(root,1)]
-        curr_depth = float('inf')
-        while stack:
-            node,depth = stack.pop()
-            if not node.right and not node.left:
-                curr_depth = min(curr_depth,depth)
-            if node.right:
-                stack.append((node.right,depth+1))
-            if node.left:
-                stack.append((node.left,depth+1))
-        return curr_depth
+        if not root.left and not root.right:
+            return 1
+        if not root.left and root.right:
+            return 1 + self.minDepth(root.right)
+        if not root.right and root.left:
+            return 1 + self.minDepth(root.left)
+        return 1 + min(self.minDepth(root.left),self.minDepth(root.right))
+    
