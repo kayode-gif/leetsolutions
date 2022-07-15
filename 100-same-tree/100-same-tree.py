882e31fcda5e6cf7ecebbce57e2e78af5b671da6
kayode-gif <s2020476@ed.ac.uk>
2022-07-15 15:53:41 +0100

    Time: 56 ms (25.90%), Space: 13.8 MB (98.28%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,21 +6,12 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        queue = [(p, q)]
-        while queue:
-            node_p, node_q = queue.pop(0)
-            #if both of nodes have values continue 
-            if not node_p and not node_q:
-                continue
-            #if atleast one of the nodes differ return False
-            if not node_p or not node_q:
-                return False
-            #if the values are the same append both of them to the queue
-            if node_p.val == node_q.val:
-                queue.append((node_p.left, node_q.left))
-                queue.append((node_p.right, node_q.right))
-            else:
-                return False
-        return True
-            
+        #recursive dfs 
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
         
