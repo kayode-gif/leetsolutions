6971536d8de05a70f26805f1cef7f5a786a6b4e9
kayode-gif <s2020476@ed.ac.uk>
2022-07-23 14:31:25 +0100

    Time: 55 ms (24.76%), Space: 13.8 MB (56.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,8 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        first, second = 1, 1
-        
-        for i in range(n):
-            first,second = second, first + second
-        
-        return first
+        stairs = {1: 1 , 2:2 , 3:3 , 4:5}
+        for i in range(4,n+1):
+            stairs[i] = stairs[i-1] + stairs[i-2]
+        return stairs[n]
