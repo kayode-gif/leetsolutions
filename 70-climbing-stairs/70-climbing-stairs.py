1e3d15f8c9417fbe80e16e0964661d58672ec3bf
kayode-gif <s2020476@ed.ac.uk>
2022-07-23 13:58:14 +0100

    Time: 53 ms (29.76%), Space: 13.9 MB (56.86%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        first, second = 1, 1
+        
+        for i in range(n):
+            first,second = second, first + second
+        
+        return first
