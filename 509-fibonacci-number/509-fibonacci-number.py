48fb362f2eda3b4ef85fd836337662a279704d4f
kayode-gif <s2020476@ed.ac.uk>
2022-07-23 14:50:25 +0100

    Time: 42 ms (73.86%), Space: 13.8 MB (53.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -7,7 +7,7 @@ class Solution:
         array = [0] * (n+1)
         array[0] = 0
         array[1] = 1
-        for i in range(2,n+1):
+        for i in range(2,n+1 ):
             array[i] = array[i-1] + array[i-2]
         return array[n]
         
