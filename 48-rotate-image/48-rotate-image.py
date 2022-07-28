7bcf5b70a83c3c2be1ed1d029deadcac957d8f99
kayode-gif <s2020476@ed.ac.uk>
2022-07-28 11:17:34 +0100

    Time: 75 ms (10.74%), Space: 14 MB (30.03%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,8 +1,12 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+        #loop through row
         for i in range(len(matrix)):
+            #then by every row look at the first index towards the end of col
             for j in range(i, len(matrix[0])):
+                #transpose so [[1,4,7],[2,5,8],[3,6,9]]
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            #reverse the row 
             matrix[i] = matrix[i][::-1]
         return matrix
         """
