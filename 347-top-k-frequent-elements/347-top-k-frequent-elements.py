e030e7e9ae78d5b485c37ec2824c2434a45fb0c6
kayode-gif <s2020476@ed.ac.uk>
2022-07-30 18:22:40 +0100

    Time: 153 ms (60.40%), Space: 18.7 MB (71.01%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,19 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # establish dictionary for frequency table 
-        # dic = {}
-        # # for numbers in the list
-        # for n in nums:
-        #     # if we havent seen the number in the dic, set to 1
-        #     if n not in dic:
-        #         dic[n] = 1
-        #     #increment the freqency 
-        #     else:
-        #         dic[n] +=1 
-        # # sort the hashmap to descend order, (key = dic.get the values)
-        # result = sorted(dic, key = dic.get, reverse = True)
-        # # return list of top elements
-        # return result[0:k]
+        dic = {}
+        # for numbers in the list
+        for n in nums:
+            # if we havent seen the number in the dic, set to 1
+            if n not in dic:
+                dic[n] = 1
+            #increment the freqency 
+            else:
+                dic[n] +=1 
+        # sort the hashmap to descend order, (key = dic.get the values)
+        result = sorted(dic, key = dic.get, reverse = True)
+        # return list of top elements
+        return result[0:k]
         
         #heap solution
         if not nums:
