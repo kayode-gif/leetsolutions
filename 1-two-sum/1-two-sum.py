f6756a3516a28fa0a1f1dcfbf5609b2d3bf0ccb9
kayode-gif <s2020476@ed.ac.uk>
2022-07-30 18:28:03 +0100

    Time: 99 ms (63.53%), Space: 15.1 MB (65.07%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashM = {}
-        for index, value in enumerate(nums):
-            diff = target - value 
-            if diff in hashM:
-                return [hashM[diff],index]
-            hashM[value] = index
+        d = {} # val:index
+        for i,value in enumerate(nums):
+            difference = target - value
+            if difference in d:
+                return [d[difference],i]
+            d[value] = i 
