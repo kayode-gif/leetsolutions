04d10b9321a335b58a859868d04c8e711025e649
kayode-gif <s2020476@ed.ac.uk>
2022-07-30 13:05:38 +0100

    Time: 447 ms (20.51%), Space: 21.7 MB (32.28%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -3,13 +3,13 @@ class Solution:
         ans = [1]*len(nums)
         left = [1]*len(nums)
         right = [1]*len(nums)
-        for i in range(1,len(nums)):
+        
+        for i in range(1, len(nums)):
             left[i] = nums[i-1]*left[i-1]
         
-        for i in range(len(nums)-2,-1,-1):
+        for i in range(len(nums)-2, -1, -1):
             right[i] = nums[i+1]*right[i+1]
-        
-        for i in range(0,len(nums)):
+        for i in range(0, len(nums)):
             ans[i] = left[i]*right[i]
-        return ans
-        
+            
+        return ans 
