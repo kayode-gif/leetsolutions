91a1e15ee6205d9abddf417888259ea49e2599a6
kayode-gif <s2020476@ed.ac.uk>
2022-07-30 22:11:48 +0100

    Time: 1031 ms (61.24%), Space: 27.4 MB (54.65%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        # set pointers to each rectangle
         left = 0
         right = len(height) - 1
         ans  = 0
         while left < right:
+            #caluate max rectangle area
             ans = max(ans, (right - left) * min(height[left],height[right]))
+            #keep shifting left and right dependant
             if height[left] < height[right]:
                 left+=1
             else:
