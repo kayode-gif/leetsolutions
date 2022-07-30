ceb6ddbf435af674510e271a5918532e7c2e0fda
kayode-gif <s2020476@ed.ac.uk>
2022-07-30 22:27:14 +0100

    Time: 1598 ms (51.03%), Space: 25 MB (38.29%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,25 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-# O(N^2) SOLUTION
-#         profit  = 0
-#         for i in range(len(prices)):
-#             for j in range(i+1,len(prices)):
-#                 if (prices[j] - prices[i] > profit):
-#                     profit = prices[j] - prices[i]
-#         return profit
-                
-#O(N) SOLUTION 
-        #holds profit
         maxP = 0
-        #set minimum buy to the first value
         buy = prices[0]
-        #loop through the array looking for "current" values less than min
-        for i in range(1,len(prices)):
-            if(prices[i] < buy):
-                #if a new minimum is found, buy it!
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
                 buy = prices[i]
             else:
-                #if a new minimum isnt, return 0 or diff between sold - min buy
                 maxP = max(maxP, prices[i] - buy)
         return maxP
-                
+            
