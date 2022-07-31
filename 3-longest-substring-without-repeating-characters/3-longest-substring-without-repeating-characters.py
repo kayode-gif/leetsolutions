98be6006d7190acb1c955173b7e9a3376eef849d
kayode-gif <s2020476@ed.ac.uk>
2022-07-31 12:29:03 +0100

    Time: 141 ms (28.12%), Space: 14.1 MB (49.48%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,13 +5,19 @@ class Solution:
         j = 0
         #create set to establish no duplicates
         charSet = set()
-        #returns max 
+        #returns longesr
         long = 0
+        #while right most pointer < length
         while j < len(s):
+            #if right most value is in set
             while s[j] in charSet:
+                #remove the left most
                 charSet.remove(s[i])
+                #increment
                 i += 1
+            #keep adding right most until condition occurs and ++
             charSet.add(s[j])
+            #return 
             long = max(long,len(charSet))
             j +=1
         return long
