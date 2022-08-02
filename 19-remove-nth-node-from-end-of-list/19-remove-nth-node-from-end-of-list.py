49acfc5b479771ac788b88f4027851e8993531dc
kayode-gif <s2020476@ed.ac.uk>
2022-08-02 15:05:32 +0100

    Time: 42 ms (76.02%), Space: 13.8 MB (70.11%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,14 +5,19 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        #create a dummy node to have "n" gap
         dummy = ListNode(47,head)
         left = dummy
+        # loop to place right pointer at n gap 
         right = head
         while n > 0 and right != None:
             right = right.next
             n = n -1 
+        # traverse list to when right and left is at desired n gap 
         while right:
             right = right.next
             left = left. next 
+        #delete so point prev to next.next
         left.next = left.next.next
+        
         return dummy.next
